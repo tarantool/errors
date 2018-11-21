ad86cdd450a625743beb3004f77cd6832b37a702
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2018-11-21 15:28:26 +0300

    Add ldoc API reference

--- errors.lua
+++ errors.lua
@@ -4,15 +4,57 @@ local log = require('log')
 local debug = require('debug')
 local checks = require('checks')
 
+--- Convenient error handling in Tarantool.
+-- @module errors
+
 if rawget(_G, "_error_classes") == nil then
     _G._error_classes = {}
 end
 
+--- The errors are represented as Lua tables of the following structure.
+-- @type error_object
+
+--- Class name.
+-- @field error_object.class_name
+
+--- Error payload.
+-- Either formatted string or any other payload passed at object creation.
+-- @field error_object.err
+
+--- String representation.
+-- This includes `class_name` and `err`.
+-- And optional `stack`, if it was not disabled for this class.
+-- @field error_object.str
+
+--- Stacktrace recordered at creation.
+-- @field error_object.stack
+
+--- Filename issued an error.
+-- @field error_object.file
+
+--- Line number issued an error.
+-- @field error_object.line
+
+--- Get string representation.
+-- The same as `tostring(error_object)`.
+-- @see error_class.tostring
+-- @function error_object:tostring
+-- @treturn string `error_object.str`
+
+--- An error class
+-- @field __type error class name
+-- @type error_class
 local error_class = {
     __type = 'error_class'
 }
 error_class.__index = error_class
 
+--- Create error object.
+-- Error payload is formatted by `string.format`
+-- @tparam[opt] number level within the meaning of Lua `error(message, level)`
+-- @tparam string fmt used for `string.format(fmt, ...)`
+-- @param[opt] ... formatting arguments
+-- @treturn error_object
 function error_class:new(...)
     checks('error_class')
 
@@ -68,6 +110,12 @@ function error_class:new(...)
     return e
 end
 
+--- Perform protected Lua call, gathering error as object
+-- @tparam function fn called function
+-- @param[opt] ... call arguments
+-- @return[1] `fn(...)` if the call succeeds without errors
+-- @treturn[2] nil
+-- @treturn[2] error_object
 function error_class:pcall(fn, ...)
     checks('error_class', '?')
 
@@ -88,10 +136,12 @@ function error_class:pcall(fn, ...)
     end
 end
 
--- expression
---   eclass:assert(cond, fmt, ...)
--- is an equivalent to
---   assert(cond, eclass:new(fmt, ...))
+--- Raise an error object unless condition is true.
+-- @param cond condition to be checked
+-- @param[opt] ... `error_class:new` args
+-- @return cond
+-- @return ...
+-- @raise (`error_object`) `error_class:new(fmt, ...)`
 function error_class:assert(cond, ...)
     checks('error_class', '?')
 
@@ -108,10 +158,23 @@ function error_class:assert(cond, ...)
     return cond, ...
 end
 
+--- Get string representation.
+-- Due to the nature of Lua, any class_name inconsistencies are ignored.
+-- @see error_object:tostring
+-- @tparam error_object err
+-- @treturn string `error_object.str`
 function error_class.tostring(err)
     return err.str
 end
 
+--- Create new error class.
+-- @within errors
+-- @function new_class
+-- @tparam string class_name
+-- @tparam ?table options Behaviour tuning options
+-- @tparam ?boolean options.capture_stack Capture backtrace at creation
+-- @tparam ?boolean options.log_on_creation Produce error log at creation
+-- @treturn error_class
 local function new_class(class_name, options)
     checks('string', {
         capture_stack = '?boolean',
