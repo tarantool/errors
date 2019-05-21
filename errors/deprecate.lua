387dd3d63326773e564563fcbca524f8b3ecedfe
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2019-05-21 21:40:50 +0300

    Optimize creation of DeprecationError

--- errors/deprecate.lua
+++ errors/deprecate.lua
@@ -14,23 +14,35 @@ end
 local function warn(message)
     checks('string')
     local level = 3 -- the function above the caller
-    local errors = require('errors')
-    local err = errors.new('DeprecationError', level, message)
+    local frame = debug.getinfo(level, "Sl")
+
+    local line = 0
+    local file = 'eval'
+
+    if type(frame) == 'table' then
+        line = frame.currentline or 0
+        file = frame.short_src or frame.source or 'eval'
+    end
 
     if errors_issued[message] == nil then
         errors_issued[message] = {}
     end
 
-    if errors_issued[message][err.file] == nil then
-        errors_issued[message][err.file] = {}
+    if errors_issued[message][file] == nil then
+        errors_issued[message][file] = {}
     end
 
     -- Don't issue same warning twice
-    if errors_issued[message][err.file][err.line] then
+    if errors_issued[message][file][line] then
         return nil
     end
 
-    errors_issued[message][err.file][err.line] = err
+    local errors = require('errors')
+    local err = errors.new('DeprecationError', level,
+        '%s (%s:%s)', message, file, line
+    )
+
+    errors_issued[message][file][line] = err
     if handler ~= nil then
         handler(err)
     else
