b01da9aa0aaa4a84ea3dcd6206ab39cd98109bd2
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2021-08-13 00:25:18 +0300

    Enhance test helpers

--- test/helper.lua
+++ test/helper.lua
@@ -10,27 +10,47 @@ end
 
 local function check_error(got, expected, subtest_name)
     local err_msg = ''
-    local pattern = '%q\ngot: %s\nexpected: %s\n'
+    local pattern = table.concat({
+        'Unexpected %s',
+        '     got: %s (%s)',
+        'expected: %s (%s)',
+        '', ''
+    }, '\n')
 
     if expected.file and expected.file ~= got.file then
-        err_msg = err_msg .. pattern:format('file', got.file, expected.file)
+        err_msg = err_msg .. pattern:format('file',
+            got.file, type(got.file),
+            expected.file, type(expected.file)
+        )
     end
 
     if expected.line and expected.line ~= got.line then
-        err_msg = err_msg .. pattern:format('line', got.line, expected.line)
+        err_msg = err_msg .. pattern:format('line',
+            got.line, type(got.line),
+            expected.line, type(expected.line)
+        )
     end
 
     if got.err ~= expected.err then
-        err_msg =  err_msg .. pattern:format('err', got.err, expected.err)
+        err_msg =  err_msg .. pattern:format('err',
+            got.err, type(got.err),
+            expected.err, type(expected.err)
+        )
     end
 
     local err_str = tostring(got)
     if err_str:match(expected.str) == nil then
-        err_msg =  err_msg .. pattern:format('tostring()', err_str, expected.str)
+        err_msg =  err_msg .. pattern:format('tostring()',
+            err_str, type(err_str),
+            expected.str, type(expected.str)
+        )
     end
 
     if tostring(got) ~= got:tostring() then
-        err_msg =  err_msg .. pattern:format('tostring()', tostring(got), got:tostring())
+        err_msg =  err_msg .. pattern:format('tostring()',
+            tostring(got), '',
+            got:tostring(), ''
+        )
     end
 
     if #err_msg ~= 0 then
